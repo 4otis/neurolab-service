57d6ffd546d8602474aac12f67e82c435af3ec9c
Khariton <khariton.crp@mail.ru>
2026-04-11 15:00:38 +0300

    AutotestUseCase was initially implemented.

--- internal/cases/autotest.go
+++ internal/cases/autotest.go
@@ -2,6 +2,9 @@ package cases
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
 
 	"github.com/4otis/neurolab-service/internal/entity"
 	"github.com/4otis/neurolab-service/internal/port/repo"
@@ -11,43 +14,110 @@ import (
 var _ AutotestUseCase = (*AutotestUseCaseImpl)(nil)
 
 type AutotestUseCase interface {
-	Process(ctx context.Context, req entity.PipelineReq) (entity.PipelineResp, error)
+	Process(ctx context.Context, req *entity.PipelineReq) (*entity.PipelineResp, error)
+}
+
+type AutotestUseCaseImpl struct {
+	logger       *zap.Logger
+	labRepo      repo.LabRepo
+	dockerRepo   repo.DockerRepo
+	solutionsDir string
+	scriptsDir   string
+	defaultImage string
+	dockerConfig map[string]string
 }
 
 func NewAutotestUseCase(
 	logger *zap.Logger,
 	labRepo repo.LabRepo,
 	dockerRepo repo.DockerRepo,
+	solutionsDir string,
+	scriptsDir string,
 ) *AutotestUseCaseImpl {
+	//TODO: вынести в отдельный конфигурационный файл .env или .yaml
+	cfg := map[string]string{
+		"python": "checker-python:1.0",
+		"golang": "checker-golang:1.0",
+		"cpp":    "checker-cpp:1.0",
+	}
+
 	return &AutotestUseCaseImpl{
-		logger:     logger,
-		labRepo:    labRepo,
-		dockerRepo: dockerRepo,
+		logger:       logger,
+		labRepo:      labRepo,
+		dockerRepo:   dockerRepo,
+		solutionsDir: solutionsDir,
+		scriptsDir:   scriptsDir,
+		defaultImage: "checker-python:1.0", // TODO: подумать о надобности
+		dockerConfig: cfg,
 	}
 }
 
-type AutotestUseCaseImpl struct {
-	logger     *zap.Logger
-	labRepo    repo.LabRepo
-	dockerRepo repo.DockerRepo
-}
+func (uc *AutotestUseCaseImpl) Process(ctx context.Context, req *entity.PipelineReq) (pipelineResp *entity.PipelineResp, err error) {
+	pipelineResp = &entity.PipelineResp{
+		IsSuccess: true,
+		StudentID: req.StudentID,
+		LabID:     req.LabID,
+		CreatedAt: req.CreatedAt,
+	}
 
-func (uc *AutotestUseCaseImpl) Process(ctx context.Context, req entity.PipelineReq) (entity.PipelineResp, error) {
-	//TODO: GET ScriptExecutePlan ("step1->step2->step3")
+	lab, err := uc.labRepo.Read(ctx, req.LabID)
+	if err != nil {
+		uc.logger.Error("failed to read pipeline execute plan",
+			zap.Int("lab_id", req.LabID),
+			zap.Error(err),
+		)
+		return pipelineResp, fmt.Errorf("failed to read pipeline execute plan:%w", err)
+	}
 
-	//TODO: UpContainer
+	scripts := strings.Split(lab.Pipeline, "->") //TODO: впоследствии поменять
+	image := uc.dockerConfig[lab.Lang]           //TODO: впоследствии поменять
 
-	//TODO: MountSolution (cases.containerUseCase? или это repo.ContainerRepo)
+	mnts := make([]*string, 0, len(scripts))
+	//mnts[0] should be solution path
+	mnts = append(mnts, &req.Path)
+	for _, s := range scripts {
+		scriptPath := uc.scriptsDir + s
+		mnts = append(mnts, &scriptPath)
+	}
 
-	// scripts := []string{"script1", "script2", "plagiarism_v1"}
-	// for _, s := range scripts {
-	// 	//TODO: MountScript()
+	// containerID, err := uc.dockerRepo.CreateContainer(ctx, image, solution, scripts)
+	containerID, err := uc.dockerRepo.CreateContainer(ctx, image, mnts)
+	if err != nil {
+		uc.logger.Error("failed to create container",
+			zap.String("image", image),
+			zap.Error(err),
+		)
+		return pipelineResp, fmt.Errorf("failed to create container:%w", err)
+	}
 
-	// 	//TODO: ExecScript()
-	// 	//TODO: Analyze status code
-	// 	//TODO: Accumulating data for PipelineResp
+	for _, s := range scripts {
+		resp, err := uc.dockerRepo.ExecCommand(ctx, containerID, []string{fmt.Sprintf("app/scripts/%s/run.sh", s)})
+		if err != nil {
+			uc.logger.Error("failed to exec command", zap.String("cmd",
+				fmt.Sprintf("app/scripts/%s/run.sh", s)),
+				zap.Error(err),
+			)
+			return pipelineResp, err
+		}
 
-	// }
+		if resp.StatusCode != 0 {
+			uc.logger.Debug("bad status code",
+				zap.String("container_id", containerID),
+				zap.String("script", s),
+				zap.Any("resp", resp),
+			)
 
-	return entity.PipelineResp{}, nil
+			pipelineResp.IsSuccess = false
+			break
+		} else {
+			uc.logger.Debug("script passed",
+				zap.String("container_id", containerID),
+				zap.String("script", s),
+				zap.Any("resp", resp),
+			)
+		}
+	}
+
+	pipelineResp.FinishedAt = time.Now()
+	return pipelineResp, nil
 }

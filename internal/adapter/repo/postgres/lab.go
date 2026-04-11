e196385efc189e90188b4c2f5c9f0b40acb5fbad
Khariton <khariton.crp@mail.ru>
2026-04-11 14:59:54 +0300

    Mocking Read func in LabRepo. Adding fixes to entities.

--- internal/adapter/repo/postgres/lab.go
+++ internal/adapter/repo/postgres/lab.go
@@ -1,6 +1,9 @@
 package postgres
 
 import (
+	"context"
+
+	"github.com/4otis/neurolab-service/internal/entity"
 	"github.com/4otis/neurolab-service/internal/port/repo"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -13,4 +16,14 @@ type LabRepo struct {
 
 func NewLabRepo(pool *pgxpool.Pool) *LabRepo {
 	return &LabRepo{pool: pool}
+}
+
+func (r *LabRepo) Read(ctx context.Context, id int) (*entity.Lab, error) {
+	return &entity.Lab{
+		ID:       id,
+		Name:     "lab name",
+		Task:     "do some task",
+		Lang:     "python",
+		Pipeline: "plagiarism_simple->python_pep8->python_lab_01_task_01",
+	}, nil
 }
--- internal/port/repo/lab.go
+++ internal/port/repo/lab.go
@@ -1,4 +1,11 @@
 package repo
 
+import (
+	"context"
+
+	"github.com/4otis/neurolab-service/internal/entity"
+)
+
 type LabRepo interface {
+	Read(ctx context.Context, id int) (*entity.Lab, error)
 }

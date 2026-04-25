418c72604007b923a78dc3dee56440e102f35dce
Eiline03 <danpost@list.ru>
2026-04-25 16:12:44 +0300

    ADD: ll-repo test version

--- internal/port/repo/llm.go
+++ internal/port/repo/llm.go
@@ -1,5 +1,18 @@
 package repo
 
+import "context"
+
+type GenerateRequest struct {
+	Prompt  string `json:"prompt"`
+	Session string `json:"session_id,omitempty"`
+}
+
+type GenerateResponse struct {
+	Content   string `json:"content"`
+	SessionID string `json:"session_id"`
+	ValidJSON bool   `json:"valid_json"`
+}
+
 type LLMRepo interface {
-	// доступные методы
+	Generate(ctx context.Context, sysP, usrP string) (rawResp string, err error)
 }
